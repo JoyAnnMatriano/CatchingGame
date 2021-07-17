06fba5020b509086fce6c1c53b2e318f4ffe2775
JoyAnnMatriano <ja.matriano24@gmail.com>
2021-07-17 18:38:31 +0800

    front menu

--- CatchingGame/Form1.Designer.cs
+++ CatchingGame/Form1.Designer.cs
@@ -38,40 +38,49 @@ namespace CatchingGame
             // title_lbl
             // 
             this.title_lbl.AutoSize = true;
-            this.title_lbl.Font = new System.Drawing.Font("Nueva Std Cond", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.title_lbl.Font = new System.Drawing.Font("my game", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.title_lbl.ForeColor = System.Drawing.Color.White;
-            this.title_lbl.Location = new System.Drawing.Point(103, 224);
+            this.title_lbl.Location = new System.Drawing.Point(35, 177);
             this.title_lbl.Name = "title_lbl";
-            this.title_lbl.Size = new System.Drawing.Size(209, 37);
+            this.title_lbl.Size = new System.Drawing.Size(319, 41);
             this.title_lbl.TabIndex = 0;
             this.title_lbl.Text = "Catching Game";
             // 
             // start_btn
             // 
+            this.start_btn.FlatAppearance.BorderSize = 2;
             this.start_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.start_btn.Font = new System.Drawing.Font("my game", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.start_btn.Font = new System.Drawing.Font("KBREINDEERGAMES", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.start_btn.ForeColor = System.Drawing.Color.White;
-            this.start_btn.Location = new System.Drawing.Point(135, 264);
+            this.start_btn.Location = new System.Drawing.Point(116, 236);
             this.start_btn.Name = "start_btn";
-            this.start_btn.Size = new System.Drawing.Size(138, 48);
+            this.start_btn.Size = new System.Drawing.Size(157, 55);
             this.start_btn.TabIndex = 1;
             this.start_btn.Text = "START";
             this.start_btn.UseVisualStyleBackColor = true;
             // 
             // help_btn
             // 
-            this.help_btn.Location = new System.Drawing.Point(151, 338);
+            this.help_btn.FlatAppearance.BorderSize = 2;
+            this.help_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.help_btn.Font = new System.Drawing.Font("KBREINDEERGAMES", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.help_btn.ForeColor = System.Drawing.Color.White;
+            this.help_btn.Location = new System.Drawing.Point(116, 297);
             this.help_btn.Name = "help_btn";
-            this.help_btn.Size = new System.Drawing.Size(105, 38);
+            this.help_btn.Size = new System.Drawing.Size(157, 50);
             this.help_btn.TabIndex = 1;
             this.help_btn.Text = "HELP";
             this.help_btn.UseVisualStyleBackColor = true;
             // 
             // quit_btn
             // 
-            this.quit_btn.Location = new System.Drawing.Point(151, 382);
+            this.quit_btn.FlatAppearance.BorderSize = 2;
+            this.quit_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.quit_btn.Font = new System.Drawing.Font("KBREINDEERGAMES", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.quit_btn.ForeColor = System.Drawing.Color.White;
+            this.quit_btn.Location = new System.Drawing.Point(116, 353);
             this.quit_btn.Name = "quit_btn";
-            this.quit_btn.Size = new System.Drawing.Size(105, 38);
+            this.quit_btn.Size = new System.Drawing.Size(157, 49);
             this.quit_btn.TabIndex = 1;
             this.quit_btn.Text = "QUIT GAME";
             this.quit_btn.UseVisualStyleBackColor = true;
